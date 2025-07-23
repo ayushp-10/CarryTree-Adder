c86d5348897a512b1a1af0d0c0d68819b8b8ef78
ayushp-10 <ayushprasad2100@gmail.com>
2025-07-23 22:08:24 +0530

    Update CarryTree.v

--- CarryTree.v
+++ CarryTree.v
@@ -3,70 +3,70 @@
 
 module kogge_stone_adder(input [3:0] A, input [3:0] B, input Cin,
                          output [3:0] Sum_KSA, output Cout_KSA);
-  wire [3:0] P = A ^ B;
-  wire [3:0] G = A & B;
-  wire [3:0] C;
+wire [3:0] P = A ^ B;
+wire [3:0] G = A & B;
+wire [3:0] C;
 
-  wire G1_0 = G[1] | (P[1] & G[0]);
-  wire G2_1 = G[2] | (P[2] & G[1]);
-  wire G3_2 = G[3] | (P[3] & G[2]);
+wire G1_0 = G[1] | (P[1] & G[0]);
+wire G2_1 = G[2] | (P[2] & G[1]);
+ wire G3_2 = G[3] | (P[3] & G[2]);
 
-  wire G2_0 = G2_1 | (P[2] & P[1] & G[0]);
+wire G2_0 = G2_1 | (P[2] & P[1] & G[0]);
   wire G3_0 = G3_2 | (P[3] & P[2] & P[1] & G[0]);
 
-  assign C[0] = Cin;
-  assign C[1] = G[0] | (P[0] & C[0]);
-  assign C[2] = G1_0 | (P[1] & P[0] & C[0]);
-  assign C[3] = G2_0 | (P[2] & P[1] & P[0] & C[0]);
-  assign Cout_KSA = G3_0 | (P[3] & P[2] & P[1] & P[0] & C[0]);
+assign C[0] = Cin;
+ assign C[1] = G[0] | (P[0] & C[0]);
+assign C[2] = G1_0 | (P[1] & P[0] & C[0]);
+assign C[3] = G2_0 | (P[2] & P[1] & P[0] & C[0]);
+assign Cout_KSA = G3_0 | (P[3] & P[2] & P[1] & P[0] & C[0]);
 
-  assign Sum_KSA = P ^ C;
+assign Sum_KSA = P ^ C;
 endmodule
 
 
 module brent_kung_adder(input [3:0] A, input [3:0] B, input Cin,
                         output [3:0] Sum_BKA, output Cout_BKA);
-  wire [3:0] P = A ^ B;
-  wire [3:0] G = A & B;
-  wire [3:0] C;
+wire [3:0] P = A ^ B;
+wire [3:0] G = A & B;
+wire [3:0] C;
 
-  wire G1_0 = G[1] | (P[1] & G[0]);
-  wire P1_0 = P[1] & P[0];
-  wire G2_0 = G[2] | (P[2] & G1_0);
-  wire P2_0 = P[2] & P1_0;
+wire G1_0 = G[1] | (P[1] & G[0]);
+wire P1_0 = P[1] & P[0];
+wire G2_0 = G[2] | (P[2] & G1_0);
+wire P2_0 = P[2] & P1_0;
   wire G3_1 = G[3] | (P[3] & G[2]);
 
-  assign C[0] = Cin;
-  assign C[1] = G[0] | (P[0] & C[0]);
+assign C[0] = Cin;
+assign C[1] = G[0] | (P[0] & C[0]);
   assign C[2] = G1_0 | (P1_0 & C[0]);
-  assign C[3] = G2_0 | (P2_0 & C[0]);
-  assign Cout_BKA = G3_1 | (P[3] & G2_0);
+assign C[3] = G2_0 | (P2_0 & C[0]);
+assign Cout_BKA = G3_1 | (P[3] & G2_0);
 
-  assign Sum_BKA = P ^ C;
+assign Sum_BKA = P ^ C;
 endmodule
 
 
 module sklansky_adder(input [3:0] A, input [3:0] B, input Cin,
                       output [3:0] Sum_SKA, output Cout_SKA);
-  wire [3:0] P = A ^ B;
-  wire [3:0] G = A & B;
-  wire [3:0] C;
+wire [3:0] P = A ^ B;
+wire [3:0] G = A & B;
+wire [3:0] C;
 
-  wire G1_0 = G[1] | (P[1] & G[0]);
-  wire P1_0 = P[1] & P[0];
-  wire G3_2 = G[3] | (P[3] & G[2]);
-  wire P3_2 = P[3] & P[2];
-  wire G2_0 = G[2] | (P[2] & G1_0);
-  wire P2_0 = P[2] & P1_0;
-  wire G3_0 = G3_2 | (P3_2 & G1_0);
+wire G1_0 = G[1] | (P[1] & G[0]);
+wire P1_0 = P[1] & P[0];
+wire G3_2 = G[3] | (P[3] & G[2]);
+wire P3_2 = P[3] & P[2];
+wire G2_0 = G[2] | (P[2] & G1_0);
+wire P2_0 = P[2] & P1_0;
+wire G3_0 = G3_2 | (P3_2 & G1_0);
 
-  assign C[0] = Cin;
-  assign C[1] = G[0] | (P[0] & C[0]);
-  assign C[2] = G1_0 | (P1_0 & C[0]);
-  assign C[3] = G2_0 | (P2_0 & C[0]);
-  assign Cout_SKA = G3_0 | (P[3] & G2_0);
+assign C[0] = Cin;
+assign C[1] = G[0] | (P[0] & C[0]);
+assign C[2] = G1_0 | (P1_0 & C[0]);
+assign C[3] = G2_0 | (P2_0 & C[0]);
+assign Cout_SKA = G3_0 | (P[3] & G2_0);
 
-  assign Sum_SKA = P ^ C;
+assign Sum_SKA = P ^ C;
 endmodule
 
 
